575ed3c4fbb84ba4f0b8321185a0b6f8920725f2
Kemo <kemoshousha309@gmail.com>
2020-10-06 07:06:30 +0200

    react app

---
+++ build/precache-manifest.06b7ae53fc70895aba359992aef85a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0ba69a943e8d8d981e4bfcd42a1101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d537f7982833ae9aa9",
+    "url": "/static/css/main.edd7be24.chunk.css"
+  },
+  {
+    "revision": "8f9f209389036621978d",
+    "url": "/static/js/2.cd88b8cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd88b8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d537f7982833ae9aa9",
+    "url": "/static/js/main.e71237a7.chunk.js"
+  },
+  {
+    "revision": "6a995a9d33cfac75b7e7",
+    "url": "/static/js/runtime-main.f1012a04.js"
+  }
+]);
